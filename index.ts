abf1599a407b59d5fe30ffe9b69e1e27617d8999
Christoffer Holst <cholst@gmail.com>
2022-09-14 19:54:48 +0200

    typo

--- index.ts
+++ index.ts
@@ -1,6 +1,5 @@
 import { serve } from "https://deno.land/std@0.140.0/http/server.ts";
 import { config } from 'https://deno.land/x/dotenv@v1.0.1/mod.ts';
-import { Config, generateSecret, on, webhooks } from "https://deno.land/x/github_webhooks@0.1.1/mod.ts";
 import { SmallBot } from "https://raw.githubusercontent.com/cybertim/SmallBotMatrix/main/mod.ts";
 
 const config = {
@@ -8,14 +7,6 @@ const config = {
   homeserverUrl: Deno.env.get("MATRIX_SERVER_NAME") || generateSecret(),
   matrixRoomId: Deno.env.get("GITHUB_WEBHOOK_ROOM_ID"),
 };
-
-const gitConf = {
-  appId: Deno.env.get("APP_ID"),
-  secret: Deno.env.get("APP_SECRET") || generateSecret(),
-  privateKey: Deno.env.get("APP_PRIVATE_KEY"),
-};
-
-
 
 const matrixBot = new SmallBot({
     accessToken: config.accessToken,
@@ -32,4 +23,6 @@ const matrixBot = new SmallBot({
 await matrixBot.start();
 
 
-serve((_req) => {});
+serve((_req) => {
+
+});
